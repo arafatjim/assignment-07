ca2348a7e3016ca5ffcc4b490de3d2c11b2029c6
arafatjim <jimyeasirarafat28@gmail.com>
2025-05-14 02:56:21 +0600

    add new design

--- src/Component/SubscribBanner/SubscribBanner.jsx
+++ src/Component/SubscribBanner/SubscribBanner.jsx
@@ -3,10 +3,10 @@ import backgroundImage from '../../assets/bg-shadow.png';
 const SubscribeBanner = () => {
           return (
                     <div className=' rounded-md pt-0 px-3 mx-auto border-2 border-[#ffffff] w-10/12 relative overflow-visible mb-[-80px]'>
-                              <div className='bg-[#c1c9ff] mx-auto  w-full  h-50   m-4 p-4 rounded-lg flex flex-col items-center justify-center text-black'  style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                             <p className='text-xl font-bold px-2 items-center justify-center'>Subscribe to our Newsletter</p> 
+                              <div className='bg-[#c1c9ff] mx-auto w-full h-50  m-4 p-4 rounded-lg flex flex-col items-center justify-center text-black'  style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                             <p className='text-xl font-bold px-2 items-center justify-center mx-auto'>Subscribe to our Newsletter</p> 
                              <p className='text-xs text-gray-500 px-2'>Get the latest updates and news right in your inbox!</p>
-                             <div className='flex gap-2 px-2 mt-4 w-46 justify-center '>
+                             <div className='flex flex-col gap-2 px-2 mt-4 w-46 justify-center lg:flex-row'>
                                         <input className='p-1 rounded-md border-2 border-gray-500' type="email" name="email" id="" placeholder='Enter Your Email Address' />
                                         <button className='bg-[#2c78f4] border-2 border-gray-500 text-white font-bold  py-1 px-2 items-center justify-center rounded-md hover:bg-orange-600'>Subscribe</button>
                              </div>
